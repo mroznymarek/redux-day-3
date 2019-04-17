eab5d6171622100c9bc1bb065f0498e4d0086ad5
Marek Mroz <marekmroz@wp.pl>
2019-04-17 14:32:54 +0200

    counter button integrated with firebase

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { incAsyncActionCreator } from './state/counter'
 
 const Counter = (props) => (
     <div>
         <h1>
             {props._number}
         </h1>
+        <button
+            onClick={props._inc}
+        >
+            +
+        </button>
     </div>
 )
 
@@ -13,6 +19,11 @@ const mapStateToProps = state => ({
     _number: state.counter.number,
 })
 
+const mapDispatchToProps = dispatch => ({
+    _inc: () => dispatch(incAsyncActionCreator())
+})
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(Counter)
--- src/state/counter.js
+++ src/state/counter.js
@@ -2,6 +2,13 @@ import { database } from '../firebaseConf'
 
 const SET = 'counter/SET'
 
+export const incAsyncActionCreator = () => (dispatch, getState) => {
+    const state = getState()
+    const currentNumber = state.counter.number
+    
+    database.ref('/counter').set(currentNumber + 1)
+}
+
 export const startCounterSyncAsyncAction = () => (dispatch, getState) => {
     database.ref('/counter').on(
         'value',
